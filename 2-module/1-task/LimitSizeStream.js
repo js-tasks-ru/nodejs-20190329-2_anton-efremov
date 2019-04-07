ce1fea540380a35f522ca101a185589d71e3f3da
I300455 <anton.efremov@sap.com>
2019-04-07 13:51:57 +1000

    add solution for 2-module 1-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,11 +4,32 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.symbolCounter = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    const str = chunk.toString(); // failed to use the encoding param here :~(
+    this.symbolCounter += chunk.length; // it could be just str.length, but we also considering symbols with double byte encoding
 
+    if (this.symbolCounter > this.limit) {
+      callback(new LimitExceededError(), null);
+    }
+
+    callback(null, str);
   }
 }
 
 module.exports = LimitSizeStream;
+
+// my local testing code below
+// const limitStream = new LimitSizeStream({limit: 3});
+// function onData(line) {
+//   console.log(line);
+// }
+
+// limitStream.on('data', onData);
+
+// limitStream.write('a');
+// limitStream.write('фg');
+// limitStream.end();
