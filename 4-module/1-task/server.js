83a212984a8c787a02c66d79dd0e436735d85add
I300455 <anton.efremov@sap.com>
2019-04-15 21:57:27 +1000

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,17 +1,36 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
+  console.log('pathname = ' + pathname);
   const filepath = path.join(__dirname, 'files', pathname);
+  console.log('filepath = ' + filepath);
 
   switch (req.method) {
     case 'GET':
 
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end();
+      } else {
+        const stream = fs.createReadStream(filepath);
+
+        stream.on('open', function () {
+          res.setHeader('content-encoding', 'utf-8');
+          stream.pipe(res);
+        });
+
+        stream.on('error', function(err) {
+          res.statusCode = 404;
+          console.log(err);
+          res.end();
+        });
+      }
       break;
 
     default:
