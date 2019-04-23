446d209ed26f083fdf071935745197ce17dae447
I300455 <anton.efremov@sap.com>
2019-04-24 01:05:05 +1000

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -6,11 +6,37 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+var subscribers = {};
 
 router.get('/subscribe', async (ctx, next) => {
+    if (ctx.method !== 'GET') return next();
+
+    var id = Math.random();
+
+    await new Promise(resolve => {
+        ctx.req.on('close', () => {
+            resolve();
+        });
+        ctx.resolve = resolve;
+        subscribers[id] = ctx;
+    });
 });
 
 router.post('/publish', async (ctx, next) => {
+    if (ctx.method !== 'POST') return next();
+    if (ctx.request.body == 'undefined' || JSON.stringify(ctx.request.body) == '{}') return next();
+
+    var message = ctx.request.body.message;
+    if (message === '') return next();
+    ctx.body = message;
+
+    for (var id in subscribers) {
+        var ctx = subscribers[id];
+        ctx.body = message;
+        ctx.resolve();
+    }
+
+    subscribers = {};
 });
 
 app.use(router.routes());
