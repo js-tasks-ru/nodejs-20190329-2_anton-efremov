8c7010dda8e10cad53b6f62675570b18b842cce5
I300455 <anton.efremov@sap.com>
2019-04-24 01:24:04 +1000

    Remove previous incorrect home tasks

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -6,6 +6,7 @@ const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
+  
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
