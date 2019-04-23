5680279366183882c612860b9a94cdb578005bee
I300455 <anton.efremov@sap.com>
2019-04-24 01:17:15 +1000

    Remove previous incorrect home tasks

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -36,25 +36,4 @@ class LineSplitStream extends stream.Transform {
   }
 }
 
-const lines = new LineSplitStream({
-  encoding: 'utf-8'
-});
-
-function onData(line) {
-  console.log(line);
-}
-
-lines.on('data', onData);
-
-// lines.write(`первая строка${os.EOL}вторая строка${os.EOL}третья строка`);
-// lines.write('a');
-// lines.write('b');
-// lines.write('c');
-// lines.write('d');
-
-
-lines.write('a');
-lines.write(`b${os.EOL}c`);
-lines.end();
-
 module.exports = LineSplitStream;
--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,7 +1,6 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,23 +11,6 @@ server.on('request', (req, res) => {
   switch (req.method) {
     case 'GET':
 
-      if (pathname.includes('/')) {
-        res.statusCode = 400;
-        res.end();
-      } else {
-        const stream = fs.createReadStream(filepath);
-
-        stream.on('open', function () {
-          res.setHeader('content-encoding', 'utf-8');
-          stream.pipe(res);
-        });
-
-        stream.on('error', function(err) {
-          res.statusCode = 404;
-          console.log(err);
-          res.end();
-        });
-      }
       break;
 
     default:
