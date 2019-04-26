08fce8ed2c560e2cc41c33c00104f89226bbd5bd
I300455 <anton.efremov@sap.com>
2019-04-26 22:10:48 +1000

    Exercise done 6-module 1-task

--- 6-module/1-task/User.js
+++ 6-module/1-task/User.js
@@ -1,5 +1,25 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({});
+const schema = new mongoose.Schema({
+    email: {
+        type: String, lowercase: true, trim: true,
+        required: true,
+        validate: [
+            {
+                validator(value) {
+                    return /^[-.\w]+@([\w-]+\.)+[\w-]{2,12}$/.test(value);
+                },
+                message: 'Email is not valid',
+            },
+        ],
+        unique: true,
+    },
+    displayName: {
+        type: String, lowercase: true, trim: true,
+        required: true,
+    }
+}, {timestamps: true});
 
-module.exports = mongoose.model('User', schema);
+schema.index({ email: 1, displayName: 1 });
+
+module.exports = mongoose.model('User', schema, 'users');
